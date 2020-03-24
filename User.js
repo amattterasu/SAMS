a24eb43a6a2b61593477448f1fcc876827e6a2a0
Andrey Gubaydullin <amattterasuuu@gmail.com>
2020-03-24 22:14:33 +0700

    1

--- User.js
+++ User.js
@@ -1,10 +1,11 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const TokenSchema = new Schema({
+const UserSchema = new Schema({
     id: Number,
-    token: String,
-    counter: Number
+    email: String,
+    password: String,
+
 });
 
 module.exports = mongoose.model('token', TokenSchema);;
--- api.js
+++ api.js
@@ -1,35 +1,35 @@
 const express = require("express");
 const router = express.Router();
-const Token = require("./Token");
+const User = require("./User");
 
-router.get("/token", (request, response) => {
+router.get("/users", (request, response) => {
     Token.find({})                        // get all data from db
-        .then(token => {
-            response.send(token);
+        .then(user => {
+            response.send(user);
         });
 });
 
-router.post("/token", (request, response) => {
+router.post("/users", (request, response) => {
     Token.create(request.body)
-        .then(token => {
-            response.send(token);
+        .then(user => {
+            response.send(user);
         });
 });
 
-router.put("/token/:id", (request, response) => {
+router.put("/users/:id", (request, response) => {
     Token.findByIdAndUpdate({_id: request.params.id}, request.body)
         .then(() => {
             Token.findOne({_id: request.params.id})
-                .then(token => {
-                    response.send(token);
+                .then(user => {
+                    response.send(user);
                 });
         });
 });
 
-router.delete("/token/:id", (request, response) => {
+router.delete("/users/:id", (request, response) => {
     Token.deleteOne({_id: request.params.id})
-        .then(token => {
-            response.send(token);
+        .then(user => {
+            response.send(user);
         });
 });
 
