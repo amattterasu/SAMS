e45baee90f4cd463f1351871e3dab42613e1fe72
Andrey Gubaydullin <amattterasuuu@gmail.com>
2020-03-25 22:17:46 +0700

    published reactapp to gh-pages

---
+++ client/build/precache-manifest.c868ce65519db0cf04b3eb622e40b63e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6732e04d161a2468d9b4f9f21bee1b14",
+    "url": "/SAMS/index.html"
+  },
+  {
+    "revision": "48a1bdd0a9b5243a7fec",
+    "url": "/SAMS/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "38ebdf51145616e8e326",
+    "url": "/SAMS/static/css/main.13d1df2d.chunk.css"
+  },
+  {
+    "revision": "48a1bdd0a9b5243a7fec",
+    "url": "/SAMS/static/js/2.0db51421.chunk.js"
+  },
+  {
+    "revision": "39b3e3119cae7356ee1c8ebcb9f42c3d",
+    "url": "/SAMS/static/js/2.0db51421.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "38ebdf51145616e8e326",
+    "url": "/SAMS/static/js/main.8cceacf3.chunk.js"
+  },
+  {
+    "revision": "75649537a78bb8005b2c",
+    "url": "/SAMS/static/js/runtime-main.ed634b2c.js"
+  }
+]);
