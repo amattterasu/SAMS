ce651fcfaf8b88090b17ede94b545893ef645a52
Andrey Gubaydullin <amattterasuuu@gmail.com>
2020-03-26 12:29:11 +0700

    added check h1

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -21,6 +21,7 @@ class App extends Component {
     render() {
         return (
             <div>
+                <h1>React App</h1>
                 <Switch>
                     <Route path="/signup"
                            render={() => <Signup/>}/>
@@ -53,7 +54,9 @@ let AppContainer = compose(
 
 const MainApp = (props) => {
     return <BrowserRouter>
-        <Provider store={store}> <AppContainer/> </Provider>
+        <Provider store={store}>
+            <AppContainer/>
+        </Provider>
     </BrowserRouter>
 }
 
