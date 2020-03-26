e4c3fe36d9145df4c6b0759a21432b1542c444c1
Andrey Gubaydullin <amattterasuuu@gmail.com>
2020-03-26 18:47:14 +0700

    published

---
+++ client/build/precache-manifest.eb648b01e82bd53ce12f68e4714550fb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "125c609e01f537a1e30f1fdb784934d9",
+    "url": "/SAMS/index.html"
+  },
+  {
+    "revision": "48a1bdd0a9b5243a7fec",
+    "url": "/SAMS/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "96a45a6454e76ed59895",
+    "url": "/SAMS/static/css/main.2ce1bd7b.chunk.css"
+  },
+  {
+    "revision": "48a1bdd0a9b5243a7fec",
+    "url": "/SAMS/static/js/2.0db51421.chunk.js"
+  },
+  {
+    "revision": "39b3e3119cae7356ee1c8ebcb9f42c3d",
+    "url": "/SAMS/static/js/2.0db51421.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "96a45a6454e76ed59895",
+    "url": "/SAMS/static/js/main.81e5d5f7.chunk.js"
+  },
+  {
+    "revision": "75649537a78bb8005b2c",
+    "url": "/SAMS/static/js/runtime-main.ed634b2c.js"
+  }
+]);
