a31bf5c4bbc8d62d6e5fb37b46f015516a607183
Andrey Gubaydullin <amattterasuuu@gmail.com>
2020-04-21 23:41:48 +0700

    Updates

---
+++ precache-manifest.8c9a0a8364ca46a53010b1f83ae7e71e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d70670539a9b46f1e27b04dc870e7a2a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ee9595c3d1c2428ce213",
+    "url": "/static/css/2.328d5c1c.chunk.css"
+  },
+  {
+    "revision": "79c23251036c7de9a865",
+    "url": "/static/css/main.eea65aa0.chunk.css"
+  },
+  {
+    "revision": "ee9595c3d1c2428ce213",
+    "url": "/static/js/2.5ee26c1c.chunk.js"
+  },
+  {
+    "revision": "eff3b4d7e4bb0087ce53139cd90bc880",
+    "url": "/static/js/2.5ee26c1c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "79c23251036c7de9a865",
+    "url": "/static/js/main.dfe011a7.chunk.js"
+  },
+  {
+    "revision": "cda345fb50b7e11c0025",
+    "url": "/static/js/runtime-main.844668cf.js"
+  }
+]);
