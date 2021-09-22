a05b96d604ff71b97d14c2c60aa1321c2aa8cdfa
Andrey Gubaydullin <amattterasuuu@gmail.com>
2021-09-22 19:08:43 +0700

    fix header

--- src/containers/QR/QR.jsx
+++ src/containers/QR/QR.jsx
@@ -4,27 +4,26 @@ import './QR.scss'
 const QRCode = require('qrcode.react')
 
 const QRCreator = ({idCode, eventsShow})  => {
-
   const phrase = eventsShow.filter(event => event.id === idCode)
 
   return (
      <div className='qrcodeContainer'>
         <QRCode
-         value={phrase[0].code}
-         size={512}
-         bgColor={"#ffffff"}
-         fgColor={"#000000"}
-         level={"L"}
-         includeMargin={false}
-         renderAs={"svg"}
-         imageSettings={{
-           src: "",
-           x: null,
-           y: null,
-           height: 24,
-           width: 24,
-           excavate: true
-         }}
+          value={phrase[0].code}
+          size={512}
+          bgColor={"#ffffff"}
+          fgColor={"#000000"}
+          level={"L"}
+          includeMargin={false}
+          renderAs={"svg"}
+          imageSettings={{
+            src: "",
+            x: null,
+            y: null,
+            height: 24,
+            width: 24,
+            excavate: true
+          }}
         />
     </div>
   )
